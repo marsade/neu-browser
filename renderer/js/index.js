e4cce92ffa8b0d337c65863cc3e52ea80a4f3ee7
marsade <adenijifara@gmail.com>
2024-07-31 14:34:41 +0300

    Fix tab highlight bug

--- renderer/js/index.js
+++ renderer/js/index.js
@@ -6,19 +6,31 @@ function addTab() {
   tabCount++;
   const tabId = `tab-${tabCount}`;
   const tabContent = `Tab ${tabCount}`;
-
+  
   // Add the new tab to the array
   tabs.push({ id: tabId, content: tabContent });
-
+  
   // Set the newly added tab as the current tab
   currentTabId = tabId;
-
+  
   // Update the DOM
-  console.log('Tabs:', tabs);
+  updateDOM();
+}
+
+// Switch tabs
+function switchToTab(tabId) {
+  if (tabId) {
+    currentTabId = tabId;
+  }
   updateDOM();
 }
 
 function removeTab(tabId) {
+
+  // Send signal to close app if tabs array is empty
+  if (tabs.length === 0) {
+    window.electronAPI.quitApp();
+  }
   // Find the tab index
   const index = tabs.findIndex(tab => tab.id === tabId);
   if (index !== -1) {
@@ -28,34 +40,23 @@ function removeTab(tabId) {
     if (currentTabId === tabId) {
       if (tabs.length > 0) {
         currentTabId = tabs[index] ? tabs[index].id : tabs[index - 1].id;
-      } else {
-        currentTabId = null;
       }
     }
     // Update the DOM
     updateDOM();
-    console.log('Tabs:', tabs);
 
-    if (tabs.length === 0) {
-      window.electronAPI.quitApp();
-    }
   }
 }
 
-function switchToTab(tabId) {
-  currentTabId = tabId;
-
-  // Update the DOM to reflect the current tab
-  updateDOM();
-  console.log('Current Tab:', currentTabId);
-}
 
 function closeTab(event) {
+  event.stopPropagation();
   const button = event.target;
   const tabDiv = button.closest('.tab');
   const tabId = tabDiv.getAttribute('data-tab-id');
   removeTab(tabId);
 }
+
 
 function updateDOM() {
   const tabContainer = document.querySelector('.tabs-container');
@@ -65,7 +66,7 @@ function updateDOM() {
     const tabDiv = document.createElement('div');
     tabDiv.className = 'tab';
     tabDiv.setAttribute('data-tab-id', tab.id);
-  tabDiv.innerHTML = `
+    tabDiv.innerHTML = `
     <div class="tab-left">
       <div class="tab-icon"></div>
         <p>New Tab</p>
@@ -76,19 +77,32 @@ function updateDOM() {
     `;
     tabContainer.appendChild(tabDiv);
 
-    // Attach event listeners to the new buttons
+    // Attach event listeners to the close buttons
     tabDiv.querySelector('.close-tab').addEventListener('click', closeTab);
+    // Attach event listeners to the tabs to listen for when they're clicked
+    tabDiv.addEventListener('click', () => switchToTab(tab.id));
+    // adjustTabWidths();
   });
-
+  
   // Highlight the current tab
   if (currentTabId) {
     const currentTabDiv = document.querySelector(`[data-tab-id="${currentTabId}"]`);
-    console.log(currentTabDiv);
     if (currentTabDiv) {
       currentTabDiv.classList.add('active');
     }
   }
 }
+
+// function adjustTabWidths() {
+//   const tabContainer = document.querySelector('.tabs-container');
+//   const tabElements = tabContainer.querySelectorAll('.tab');
+//   const containerWidth = tabContainer.clientWidth;
+//   const tabWidth = Math.max(containerWidth / tabs.length, 100); // Minimum width of 100px per tab
+//   tabElements.forEach(tab => {
+//     tab.style.width = `${tabWidth}px`;
+//   });
+// }
+
 
 //Load a tab on intitial window load
 window.onload = () => {
