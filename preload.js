1992d65698fdeaacdbf12a8372ccad7d73ec35fb
marsade <adenijifara@gmail.com>
2024-08-07 15:09:50 +0300

    Add Shopify draggable

--- preload.js
+++ preload.js
@@ -1,5 +1,13 @@
 const { contextBridge, ipcRenderer } = require('electron');
+const { Draggable } = require('@shopify/draggable');
+
 
 contextBridge.exposeInMainWorld('electronAPI', {
   quitApp: () => ipcRenderer.send('quit-app')
+});
+
+contextBridge.exposeInMainWorld('draggable', {
+  create: (selectors, options) => {
+    return new Draggable(document.querySelectorAll(selectors), options);
+  }
 });
