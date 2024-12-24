6ade9b3d95c879c5e5fbba9311b6ba68ec5999dc
Mars Adeniji <adenijifara@gmail.com>
2024-12-24 18:28:22 +0200

    Link debounce function to electron API

--- preload.js
+++ preload.js
@@ -1,10 +1,12 @@
 const { contextBridge, ipcRenderer } = require('electron');
+const debounce = require('./renderer/js/utils')
 const { Draggable } = require('@shopify/draggable');
 
 
 contextBridge.exposeInMainWorld('electronAPI', {
   quitApp: () => ipcRenderer.send('quit-app'),
-  toggleLock: (state) => ipcRenderer.send('toggle-lock', state)
+  toggleLock: (state) => ipcRenderer.send('toggle-lock', state),
+  debounce,
 });
 
 contextBridge.exposeInMainWorld('draggable', {
