9eb80348bbca4539cf230533b6cd19e74021d66b
Mars Adeniji <adenijifara@gmail.com>
2024-12-27 17:47:00 +0200

    Minor changes

--- preload.js
+++ preload.js
@@ -1,12 +1,13 @@
 const { contextBridge, ipcRenderer } = require('electron');
-const debounce = require('./renderer/js/utils')
+const debounce = require('./renderer/js/utils');
 const { Draggable } = require('@shopify/draggable');
-
+const path = require('path');
 
 contextBridge.exposeInMainWorld('electronAPI', {
   quitApp: () => ipcRenderer.send('quit-app'),
   toggleLock: (state) => ipcRenderer.send('toggle-lock', state),
   debounce,
+  getHomePath: () => path.join(__dirname, '/renderer', 'home.html')
 });
 
 contextBridge.exposeInMainWorld('draggable', {
