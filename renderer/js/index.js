19c483f56bfee7f7b07a5f94ec335a6cce7b0766
mars <mars@gmail.com>
2025-01-24 22:07:01 +0200

    Fix bugs

--- renderer/js/index.js
+++ renderer/js/index.js
@@ -1,6 +1,5 @@
 const tabs = [];
 const addressBar = document.getElementById('address-bar');
-const searchHome = document.getElementById('search-home');
 const suggestionBox = document.getElementById('suggestion-box');
 let tabCount = 0;
 let currentWebView = null;
@@ -9,25 +8,76 @@ let currentTabId = null;
 // Insert a new tab
 function createTab () {
   tabCount++;
+  console.log('tabCount:', tabCount);
   const tabId = `tab-${tabCount}`;
-  const history = [];
-  history.push(`file://${window.electronAPI.getHomePath()}`);
   currentTabId = tabId;
-  tabs.push({ id: tabId, history });
-  if (tabCount > 1) {
-    const webview = createWebView(tabId);
-  }
-  console.log(tabs);
+  
+  const homepageDiv = document.createElement('div');
+  homepageDiv.setAttribute('class', 'home')
+  homepageDiv.setAttribute('id', `homepage-${tabId}`);
+  homepageDiv.innerHTML = `
+    <div class="logo-container">
+    <div class="logo-img"></div>
+    <div class="logo-text"></div>
+    </div>
+    <div class="search-main">
+        <input type="text" class="search-home" id="search-home" placeholder="Search">
+    </div>
+    <div class="popular">
+        <p>Popular</p>
+        <div class="pop-list">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+        </div>
+        `;
+  const webviewContainer = document.querySelector('.webview-container')
+  tabs.push({ id: tabId, history: [], content: homepageDiv });
+  currentWebView = null;
+  const parentDiv = webviewContainer.parentNode;
+  parentDiv.insertBefore(homepageDiv, webviewContainer);
+  addressBar.value = '';
+  const searchHome = document.getElementById('search-home');
+  addressBar.focus();
+  
+  searchHome.addEventListener('input', (event) => {
+    addressBar.focus();
+    addressBar.click();
+    addressBar.value = event.target.value;
+    searchHome.value = '';
+  });
+  console.log('Called by createTab');
   updateDOM();
 }
 
 // Switch tabs
-function switchToTab (tabId) {
-  if (tabId) {
-    currentTabId = tabId;
-    currentWebView = document.querySelector(`webview[data-tab-id="${tabId}"]`);
+function updateAddressBar () {
+  if (currentWebView && currentWebView !== null) {
+    console.log('current webview:', currentWebView.src);
+    addressBar.value = currentWebView.src;
+  } else {
+    addressBar.value = '';
   }
-  updateDOM();
+}
+
+function switchToTab (tabId) {
+  if (currentTabId !== tabId) {
+    currentTabId = tabId;
+    const currentTab = tabs.find(tab => tab.id === tabId);
+    addressBar.focus();
+    if (currentTab) {
+      if (currentTab.content instanceof HTMLElement && currentTab.content.tagName === 'WEBVIEW') {
+        currentWebView = currentTab.content;
+      } else {
+        currentWebView = null;
+      }
+      updateAddressBar();
+    }
+    updateDOM();
+  }
 }
 
 // Retrieve the tab to close
@@ -36,14 +86,22 @@ function closeTab (event) {
   const button = event.target;
   const tabDiv = button.closest('.tab');
   const tabId = tabDiv.getAttribute('data-tab-id');
+  tabCount--;
+  console.log('tabCount:', tabCount);
+
   removeTab(tabId);
+  updateAddressBar();
   removeWebView(tabId);
 }
 
 // Remove tab from the DOM
 function removeTab (tabId) {
-  // Find the tab index
   const index = tabs.findIndex(tab => tab.id === tabId);
+  const homePageDiv = document.querySelector(`#homepage-${tabId}`)
+
+  if (homePageDiv) {
+    homePageDiv.remove();
+  }
   if (index !== -1) {
     tabs.splice(index, 1);
 
@@ -51,7 +109,14 @@ function removeTab (tabId) {
     if (currentTabId === tabId) {
       if (tabs.length > 0) {
         currentTabId = tabs[index] ? tabs[index].id : tabs[index - 1].id;
+        const currentTab = tabs.find(tab => tab.id === currentTabId);
+        if (currentTab) {
+          currentWebView = currentTab.content;
+        }
       }
+    } else {
+      newCount = tabs.slice(-1)[0].id;
+      tabCount = Number(newCount.split('-')[1]);
     }
     if (tabs.length === 0) {
       window.electronAPI.quitApp();
@@ -61,22 +126,41 @@ function removeTab (tabId) {
 }
 
 // Create the webview for each tab
-function createWebView(tabId, url = `file://${window.electronAPI.getHomePath()}`) {
+function createWebView(tabId, url) {
   const webView = document.createElement('webview');
   const homepage = document.querySelector('.home');
+  const homePageDiv = document.querySelector(`#homepage-${tabId}`)
   const webviewContainer = document.querySelector('.webview-container');
+
+  if (homePageDiv) {
+    homePageDiv.remove();
+  }
+
   webView.setAttribute('src', url);
   webView.setAttribute('data-tab-id', tabId);
   webView.classList.add('webview');
-  homepage.style.display = 'none';
   webviewContainer.appendChild(webView);
+
+  const currentTab = tabs.find(tab => tab.id === tabId);
+  if (currentTab) {
+    currentTab.content = webView;
+  }
   currentWebView = webView;
+  console.log('Current Tab Info', currentTab);
+
   currentWebView.addEventListener('did-finish-load', () => {
-    let tabsMap = new Map(tabs.map(tab => [tab.id, tab]));
-    const currentTab = tabsMap.get(currentTabId);
-    currentTab.history.push(currentWebView.src);
-    console.log(currentTab.history);
+    if (currentTab && currentWebView) {
+      currentTab.history.push(currentWebView.src);
+    }
   });
+  currentWebView.addEventListener('page-title-updated', (event) => {
+    if (currentTab) {
+      currentTab.title = event.title;
+      updateDOM();
+    }
+  });
+  console.log('Called by createWebView');
+  updateDOM();
   return webView;
 }
 
@@ -94,7 +178,7 @@ function adjustTabWidths () {
   const tabElements = tabContainer.querySelectorAll('.tab');
   const containerWidth = tabContainer.clientWidth;
   if (tabs.length > 5) {
-    const tabWidth = Math.max(containerWidth / tabs.length, 160); // Minimum width of 100px per tab
+    const tabWidth = Math.max(containerWidth / tabs.length, 160); 
     tabElements.forEach(tab => {
       tab.style.width = `${tabWidth}px`;
     });
@@ -103,22 +187,25 @@ function adjustTabWidths () {
 
 // Update the DOM if changes
 function updateDOM () {
+  console.log('Tabs array', tabs);
   const tabContainer = document.querySelector('.tabs-container');
   tabContainer.innerHTML = '';
   tabs.forEach(tab => {
     const tabDiv = document.createElement('div');
     tabDiv.className = 'tab';
     tabDiv.setAttribute('data-tab-id', tab.id);
+
+    content = tab.content;
     tabDiv.innerHTML = `
     <div class="tab-left">
       <div class="tab-icon"></div>
-        <p>New Tab</p>
+      <p>${tab.title || 'New Tab'}</p>
     </div>
     <button class="close-tab">
-      <img src="imgs/icons/close_line.svg" width="15">
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512"><path fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M368 368L144 144m224 0L144 368"/></svg>
     </button>
     `;
-    // webview = tab.content;
+
     tabContainer.appendChild(tabDiv);
     tabDiv.addEventListener('mousedown', () => switchToTab(tab.id));
     tabDiv.querySelector('.close-tab').addEventListener('click', closeTab);
@@ -127,22 +214,21 @@ function updateDOM () {
     });
     //   adjustTabWidths();
 
-  //   if (tab.id === currentTabId) {
-  //     webview.style.visibility = 'visible';
-  //     webview.style.position = 'relative';
-  //     webview.style.zIndex = 1;
-  //   } else {
-  //     webview.style.visibility = 'hidden';
-  //     webview.style.position = 'absolute';
-  //     webview.style.zIndex = -1;
-  //   }
+    if (tab.id === currentTabId) {
+      content.style.visibility = 'visible';
+      content.style.zIndex = 1;
+    } else {
+      content.style.visibility = 'hidden';
+      content.style.position = 'absolute';
+      content.style.zIndex = -1;
+    }
   });
 
   // Highlight the current tab
   if (currentTabId) {
     const currentTabDiv = document.querySelector(`[data-tab-id="${currentTabId}"]`);
     const currentWebView = document.querySelector(`webview[data-tab-id="${currentTabId}"]`);
-    // currentTabDiv.classList.add('active');
+    currentTabDiv.classList.add('active');
   }
 }
 
@@ -161,12 +247,6 @@ const fetchSuggestions = window.electronAPI.debounce(async (query) => {
   }
 }, 300);
 
-searchHome.addEventListener('input', (event) => {
-  addressBar.focus();
-  addressBar.click();
-  addressBar.value = event.target.value;
-  searchHome.value = '';
-});
 
 addressBar.addEventListener('input', (event) => {
   const query = event.target.value;
@@ -179,6 +259,7 @@ addressBar.addEventListener('click', function () {
 
 function displaySuggestions (suggestions) {
   suggestionBox.innerHTML = '';
+  suggestionBox.style.zIndex = 9999;
   suggestions.forEach((suggestion) => {
     const suggestionItem = document.createElement('div');
     suggestionItem.className = 'suggestion-item';
@@ -199,12 +280,16 @@ function sendQuery (userInput) {
   const searchQuery = encodeURIComponent(userInput);
   const searchURL = `https://www.google.com/search?q=${searchQuery}`;
   addressBar.value = searchURL;
-  if (currentWebView) {
+
+  const currentTab = tabs.find(tab => tab.content === currentTabId);
+  if (currentTab && currentTab.content instanceof HTMLElement && currentTab.content.tagName === 'WEBVIEW') {
+
     currentWebView.setAttribute('src', searchURL);
   } else {
     createWebView(currentTabId, searchURL);
   }
 }
+
 
 // Load a tab on intitial window load
 window.onload = () => {
