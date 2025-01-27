fb43f9cf6b824ff9fa7ff55330ad503fc915750a
marsade <adenijifara@gmail.com>
2025-01-27 15:16:19 +0200

    Add global shortcuts and quit functionalities

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-const { app, BrowserWindow, Menu, ipcMain, globalShortcut} = require('electron');
+const { app, BrowserWindow, Menu, ipcMain, globalShortcut, dialog, Notification} = require('electron');
 const path = require('path');
 const isDev = process.env.NODE_ENV !== 'production';
-let isLocked = false;
+let isLocked = true;
 let mainWindow;
+let quitTimer;
 
 function createMainWindow () {
   mainWindow = new BrowserWindow({
@@ -24,27 +25,71 @@ function createMainWindow () {
     mainWindow.webContents.openDevTools();
   }
 
-  globalShortcut.register('Control+Shift+Q', () => {
-    app.quit();
-  });
-
-  // mainWindow.on('blur', () => {
-  //   console.log('App lost focus');
-  //   if (isLocked) {
-  //     console.log('App is locked and not in focus. Quitting...');
-  //     app.quit();
-  //   }
-  // });
-
-  mainWindow.on('close', (event) => {
+  
+  mainWindow.on('focus', () => {
     if (isLocked) {
-      event.preventDefault();
-      mainWindow.setMovable(false);
+      cancelQuit();
     }
   });
+
+  mainWindow.on('blur', () => {
+    if (isLocked) {
+      warnAndQuit();
+    }
+  });
+
+  mainWindow.on('minimize', () => {
+    if (isLocked) {
+      warnAndQuit();
+    }
+  });
+
   mainWindow.loadFile('renderer/index.html');
   mainWindow.maximize();
   mainWindow.on('closed', () => (mainWindow = null));
+  registerShortcuts();
+}
+
+function registerShortcuts() {
+  globalShortcut.register('Control+Shift+Q', () => {
+    app.quit();
+  });
+  globalShortcut.register('Control+Shift+D+X', () => {
+    if (isLocked) {
+      isLocked = false;
+      showNotification('Locks disabled', 'Browser locks disabled');
+    }
+  })
+}
+
+function showNotification(title, body) {
+  new Notification({ title, body }).show();
+}
+
+function cancelQuit() {
+  if (quitTimer) {
+    clearTimeout(quitTimer);
+    console.log('Quit process canceled as the app regained focus.');
+  }
+}
+
+function warnAndQuit() {
+  clearTimeout(quitTimer);
+  
+  dialog.showMessageBox(
+    mainWindow, 
+    {
+      type: 'warning',
+      title: 'NEU Browser',
+      message: 'Browser is out of focus, Quitting in 5 seconds',
+      buttons: ['Quit', 'Cancel']
+    }
+  ).then((result) => {
+    if (result.response === 0) {
+      app.quit();
+    }
+  });
+  quitTimer = setTimeout(() => app.quit(), 5000);
 }
 ipcMain.on('quit-app', () => {
   app.quit();
