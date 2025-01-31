ac1e0521f04b2f45287cca05175d22e4fa77ba1c
marsade <adenijifara@gmail.com>
2025-01-31 02:22:04 +0200

    Add nav bar buttons functionality

--- renderer/js/index.js
+++ renderer/js/index.js
@@ -6,7 +6,7 @@ const popLinks = [
   { url: 'https://www.neu.edu.tr/', title: 'NEU Website' },
   { url: 'https://www.instagram.com/', title: 'NEU Instagram' },
   { url: 'http://library.neu.edu.tr/cgi-bin/koha/opac-main.pl', title: 'NEU Library' },
-  { url: 'https://permissions.gov.ct.tr/', title: 'Student Permission' },
+  { url: 'https://permissions.gov.ct.tr/login', title: 'Student Permission' },
 ]
 let tabCount = 0;
 let currentWebView = null;
@@ -15,7 +15,6 @@ let currentTabId = null;
 // Add a new tab
 function createTab () {
   tabCount++;
-  console.log('tabCount:', tabCount);
   const tabId = `tab-${tabCount}`;
   currentTabId = tabId;
   
@@ -36,11 +35,25 @@ function createTab () {
   </div>
   `;
 
-  const webviewContainer = document.querySelector('.webview-container')
-  tabs.push({ id: tabId, history: [], content: homepageDiv });
+  tabs.push({ id: tabId, history: [], content: homepageDiv, currentHistoryIndex: 0, addressBarValue: '' });
   currentWebView = null;
+  const webviewContainer = document.querySelector('.webview-container')
   const parentDiv = webviewContainer.parentNode;
   parentDiv.insertBefore(homepageDiv, webviewContainer);
+  addressBar.classList.remove("animated");
+
+
+  const currentTab = tabs.find((tab) => tab.id === tabId);
+  if (currentTab) {
+    currentTab.history.push(homepageDiv)
+  }
+  let hasPlayed = false;
+  if (!hasPlayed) {
+    addressBar.classList.remove("animated"); 
+    void addressBar.offsetWidth; 
+    addressBar.classList.add("animated");
+    hasPlayed = true;
+  }
   addressBar.value = '';
   addressBar.focus();
   
@@ -51,14 +64,12 @@ function createTab () {
     addressBar.value = event.target.value;
     searchHome.value = '';
   });
-  console.log('Called by createTab');
   updateAllHomePages();
   updateDOM();
 }
 
 function updateAllHomePages () {
   const homePages = document.querySelectorAll('.home');
-  console.log(homePages);
   homePages.forEach((homePage) => {
     
     let popList = homePage.querySelector('.pop-list');
@@ -72,15 +83,14 @@ function updateAllHomePages () {
     popLinks.forEach((link) => {
       const popItem = document.createElement('div');
       const popImg = document.createElement('div');
-      popImg.style.backgroundImage = `url(${getFavicon(link.url)})`;
-      popItem.appendChild(popImg);
-
       const label = document.createElement('span');
-      label.textContent = link.title;
-      label.className = 'pop-label';
-      popItem.appendChild(label);
       popItem.className = 'pop-item';
       popItem.title = link.title;
+      popImg.style.backgroundImage = `url(${getFavicon(link.url)})`;
+      label.textContent = link.title;
+      label.className = 'pop-label';
+      popItem.appendChild(popImg);
+      popItem.appendChild(label);
   
       popItem.addEventListener('click', () => {
         createWebView(currentTabId, link.url);
@@ -91,13 +101,14 @@ function updateAllHomePages () {
 }
 
 function getFavicon (url) {
-  return `https://www.google.com/s2/favicons?sz=128&domain=${url}`;
+  return `https://www.google.com/s2/favicons?sz=64&domain=${url}`;
 }
 
 function updateAddressBar () {
-  if (currentWebView && currentWebView !== null) {
+  const currentTab = tabs.find(tab => tab.id === currentTabId);
+  if (currentWebView == currentTab.content && currentWebView) {
     console.log('current webview:', currentWebView.src);
-    addressBar.value = currentWebView.src;
+    currentTab.addressBarValue = currentWebView.src;
   } else {
     addressBar.value = '';
   }
@@ -105,17 +116,18 @@ function updateAddressBar () {
 
 // Switch tabs
 function switchToTab (tabId) {
+  console.log('Switching');
+  addressBar.focus();
   if (currentTabId !== tabId) {
     currentTabId = tabId;
     const currentTab = tabs.find(tab => tab.id === tabId);
-    addressBar.focus();
     if (currentTab) {
       if (currentTab.content instanceof HTMLElement && currentTab.content.tagName === 'WEBVIEW') {
         currentWebView = currentTab.content;
       } else {
         currentWebView = null;
+        addressBar.focus();
       }
-      updateAddressBar();
     }
     updateDOM();
   }
@@ -127,8 +139,6 @@ function closeTab (event) {
   const button = event.target;
   const tabDiv = button.closest('.tab');
   const tabId = tabDiv.getAttribute('data-tab-id');
-  tabCount--;
-  console.log('tabCount:', tabCount);
 
   removeTab(tabId);
   updateAddressBar();
@@ -148,16 +158,19 @@ function removeTab (tabId) {
 
     // If the current tab is removed, set the current tab to the next available tab
     if (currentTabId === tabId) {
+      console.log('tabCount for closing current tab:', tabCount);
       if (tabs.length > 0) {
         currentTabId = tabs[index] ? tabs[index].id : tabs[index - 1].id;
         const currentTab = tabs.find(tab => tab.id === currentTabId);
-        if (currentTab) {
+        if (currentTab.content instanceof HTMLElement && currentTab.content.tagName === 'WEBVIEW') {
           currentWebView = currentTab.content;
         }
       }
     } else {
+      console.log('Remove not current tab from DOM');
       newCount = tabs.slice(-1)[0].id;
       tabCount = Number(newCount.split('-')[1]);
+      console.log('tabCount for closing not current tab:', tabCount);
     }
     if (tabs.length === 0) {
       window.electronAPI.quitApp();
@@ -168,41 +181,73 @@ function removeTab (tabId) {
 
 // Create the webview for each tab
 function createWebView(tabId, url) {
-  const webView = document.createElement('webview');
-  const homepage = document.querySelector('.home');
+  let webView = document.querySelector(`webview[data-tab-id="${tabId}"]`);
   const homePageDiv = document.querySelector(`#homepage-${tabId}`)
   const webviewContainer = document.querySelector('.webview-container');
 
   if (homePageDiv) {
-    homePageDiv.remove();
+    homePageDiv.style.visibility = 'hidden';
+    homePageDiv.style.display = 'none';
   }
   
-  webView.setAttribute('src', url);
-  webView.setAttribute('data-tab-id', tabId);
-  webView.classList.add('webview');
-  webviewContainer.appendChild(webView);
+  if (!webView) {
+    webView = document.createElement('webview');
+    webView.setAttribute('src', url);
+    webView.setAttribute('data-tab-id', tabId);
+    webView.classList.add('webview');
+    webviewContainer.appendChild(webView);
+  } else {
+    webView.setAttribute('src', url);
+    // webView.style.display = 'block';
+  }
   
   const currentTab = tabs.find(tab => tab.id === tabId);
   if (currentTab) {
     currentTab.content = webView;
   }
+  currentTab.history.splice(1);
   currentWebView = webView;
-  console.log('Current Tab Info', currentTab);
+  trackWebView();
+}
 
-  currentWebView.addEventListener('did-finish-load', () => {
-    if (currentTab && currentWebView) {
-      currentTab.history.push(currentWebView.src);
+function trackWebView() {
+  const currentTab = tabs.find(tab => tab.id === currentTabId);
+  if (!currentTab) return;
+  if (!currentWebView) return;
+  let lastURL = '';
+
+  currentWebView.addEventListener('did-navigate', (event) => {
+    if (currentWebView) {
+      lastURL = event.url;
+      if (!currentTab.history.includes(event.url)) {
+        currentTab.history.push(event.url);
+        currentTab.currentHistoryIndex = currentTab.history.length - 1;
+        updateDOM();
+        console.log('Navigate');
+      }
     }
   });
+
+  currentWebView.addEventListener('did-navigate-in-page', (event) => {
+    if (currentWebView && event.url !== lastURL) {
+      if (!currentTab.history.includes(event.url)) {
+        currentTab.history.push(event.url);
+        currentTab.currentHistoryIndex++;
+        updateDOM();
+        console.log('Navigate in page');
+        lastURL = event.url;
+      }
+    }
+    console.log('Last URL visited ' + lastURL);
+
+  });
+
   currentWebView.addEventListener('page-title-updated', (event) => {
     if (currentTab) {
       currentTab.title = event.title;
       updateDOM();
     }
   });
-  console.log('Called by createWebView');
-  updateDOM();
-  return webView;
 }
 
 // Remove the webview for the given tab
@@ -236,14 +281,13 @@ function updateDOM () {
     tabDiv.className = 'tab';
     tabDiv.setAttribute('data-tab-id', tab.id);
 
-    content = tab.content;
     tabDiv.innerHTML = `
     <div class="tab-left">
     <div class="tab-icon"></div>
     <p>${tab.title || 'New Tab'}</p>
     </div>
     <button class="close-tab">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512"><path fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M368 368L144 144m224 0L144 368"/></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512"><path fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M368 368L144 144m224 0L144 368"/></svg>
     </button>
     `;
     
@@ -255,21 +299,22 @@ function updateDOM () {
     });
     //   adjustTabWidths();
     
+    updateAddressBar();
+    let content = tab.content;
     if (tab.id === currentTabId) {
       content.style.visibility = 'visible';
       content.style.zIndex = 1;
+      addressBar.value = tab.addressBarValue;
     } else {
       content.style.visibility = 'hidden';
       content.style.position = 'absolute';
       content.style.zIndex = -1;
     }
-
   });
 
   // Highlight the current tab
   if (currentTabId) {
     const currentTabDiv = document.querySelector(`[data-tab-id="${currentTabId}"]`);
-    const currentWebView = document.querySelector(`webview[data-tab-id="${currentTabId}"]`);
     currentTabDiv.classList.add('active');
   }
 }
@@ -289,16 +334,14 @@ const fetchSuggestions = window.electronAPI.debounce(async (query) => {
   }
 }, 300);
 
-
-addressBar.addEventListener('input', (event) => {
-  const query = event.target.value;
-  fetchSuggestions(query);
-});
-
-addressBar.addEventListener('click', function () {
-  this.classList.add('clicked');
-  this.select();
-});
+function isValidURL(input) {
+  try {
+    const url = new URL(input);
+    return url.protocol === 'http:' || url.protocol === 'https:'; 
+  } catch (e) {
+    return false; 
+  }
+}
 
 function displaySuggestions (suggestions) {
   suggestionBox.innerHTML = '';
@@ -320,25 +363,73 @@ function displaySuggestions (suggestions) {
 
 // Send the search query to the current webview or create a new tab if none is open
 function sendQuery (userInput) {
-  const searchQuery = encodeURIComponent(userInput);
-  const searchURL = `https://www.google.com/search?q=${searchQuery}`;
-  addressBar.value = searchURL;
+  let searchURL;
+  if (isValidURL(userInput)) {
+    searchURL = userInput;
+  } else {
+    const searchQuery = encodeURIComponent(userInput);
+    searchURL = `https://www.google.com/search?q=${searchQuery}`;
+  }
   
-  const currentTab = tabs.find(tab => tab.content === currentTabId);
+  const currentTab = tabs.find(tab => tab.id === currentTabId);
   if (currentTab && currentTab.content instanceof HTMLElement && currentTab.content.tagName === 'WEBVIEW') {
-    
     currentWebView.setAttribute('src', searchURL);
   } else {
     createWebView(currentTabId, searchURL);
   }
 }
 
+function goBackInTab(tabId){
+  const currentTab = tabs.find(tab => tab.id === tabId);
+  if (!currentTab) return;
+  if (currentTab.currentHistoryIndex <= 0) return;
+  currentTab.currentHistoryIndex--;
+  
+  const prevEntry = currentTab.history[currentTab.currentHistoryIndex];
+  const webview = document.querySelector(`webview[data-tab-id="${tabId}"]`);
+  const homePageDiv = document.querySelector(`#homepage-${tabId}`);
+
+  if (prevEntry instanceof HTMLElement && prevEntry.classList.contains('home')) {
+    if (webview){
+      webview.remove();
+      currentWebView = null;
+    }
+    if (homePageDiv) {
+      homePageDiv.style.display = 'block';
+      homePageDiv.style.visibility = 'visible';
+      currentTab.title = 'New Tab';
+      currentTab.addressBarValue = '';
+      currentTab.content = homePageDiv;
+    }
+  } else {
+    webview.setAttribute('src', prevEntry);
+    if (homePageDiv) homePageDiv.style.display = 'none';
+  }
+  updateDOM();
+}
+
+function goForwardInTab(tabId){
+  const currentTab = tabs.find(tab => tab.id === tabId);
+  if (!currentTab) return;
+  if (currentTab.currentHistoryIndex >= currentTab.history.length - 1) return;
+  currentTab.currentHistoryIndex++;
+  console.log('Forward tab index', currentTab.currentHistoryIndex);
+
+  const nextEntry = currentTab.history[currentTab.currentHistoryIndex];
+  const prevEntry = currentTab.history[currentTab.currentHistoryIndex - 1];
+  const webview = document.querySelector(`webview[data-tab-id="${tabId}"]`);
+  if (prevEntry instanceof HTMLElement && prevEntry.classList.contains('home')) {
+    createWebView(tabId, nextEntry);
+  } else {
+    webview.setAttribute('src', nextEntry);
+  }
+  updateDOM();
+}
 // Load a tab on intitial window load
 window.onload = () => {
   createTab();
   const lockButton = document.querySelector('.lock');
   const searchInput = document.getElementById('search-input');
-  
   const backButton = document.querySelector('.nav-prev');
   const forwardButton = document.querySelector('.nav-next');
   const reloadButton = document.querySelector('.nav-rel');
@@ -354,6 +445,28 @@ window.onload = () => {
   });
   
   backButton.addEventListener('click', () => {
-    console.log(currentWebView.canGoBack());
+    goBackInTab(currentTabId); 
+  });
+
+  forwardButton.addEventListener('click', () => {
+    goForwardInTab(currentTabId);
+  });
+  
+  reloadButton.addEventListener('click', () => {
+    const currentTab = tabs.find(tab => tab.id === currentTabId);
+    if (currentTab && currentTab.content instanceof HTMLElement && currentTab.content.tagName === 'WEBVIEW') {
+      currentWebView.reload();
+    }
+    updateDOM();
+  }); 
+
+  addressBar.addEventListener('input', (event) => {
+    const query = event.target.value;
+    fetchSuggestions(query);
+  });
+  
+  addressBar.addEventListener('click', function () {
+    this.classList.add('animated');
+    this.select();
   });
 };
